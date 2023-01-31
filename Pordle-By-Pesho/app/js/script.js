b3d5d7459c924b09e5b4f0ad14d2183ceed500aa
chrisAXZA <nexter1@tutanota.com>
2023-01-31 20:47:25 +0200

    Add updateLetters in script.js

--- Pordle-By-Pesho/app/js/script.js
+++ Pordle-By-Pesho/app/js/script.js
@@ -1,11 +1,24 @@
 const lettersPattern = /^[a-zA-Z]{1}$/;
+let currentGuessCount = 1;
+let currentGuess = document.querySelector(`#guess${currentGuessCount}`);
 
 // detect keypress (letter, backspace, other)
 document.addEventListener('keydown', (event) => {
-    const isLetter = lettersPattern.test(event.key);
-    const isBackspace = event.key === "Backspace";
+    const keypress = event.key;
+    const isLetter = lettersPattern.test(keypress);
+    const isBackspace = keypress === "Backspace";
 
-    // console.log(event.key);
+    // console.log('key', event.key);
     // console.log('isLetter', isLetter);
     // console.log('isBackspace', isBackspace);
+
+    if (isLetter) {
+        updateLetters(keypress);
+    }
 });
+
+function updateLetters(letter) {
+    // let currentLetters = currentGuess.dataset.letters;
+    // currentLetters += letter;
+    currentGuess.dataset.letters += letter;
+}
